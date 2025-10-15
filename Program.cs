6bdea5a7a6ee459f44608f73194389236523bb86
XD0uc3tt3 <doucettexander@gmail.com>
2025-10-15 14:09:07 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMap();
+        }
+
+        static void PrintMap()
+        {
+            char[,] map = new char[,]
+            {
+                {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
+                {'-', '^', '-', '^', '-', '-', '-', '-', '-', '^', '-', '^', '-'},
+                {'^', '^', '^', '^', '^', '-', '-', '-', '^', '^', '^', '^', '^'},
+                {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
+                {'~', '~', '~', '~', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
+                {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+                {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+                {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'}
+            };
+
+            int rows = map.GetLength(0);
+            int cols = map.GetLength(1);
+
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    Console.Write(map[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Virus()
+        {
+            //
+        }
+    }
+}
